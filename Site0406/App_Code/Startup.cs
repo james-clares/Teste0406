ccb2f0bac9378b224d43a037c217aa6792c3b8c7
James Clares <james.clares@clarifysistemas.com.br>
2019-06-04 23:48:43 -0300

    Adicionar arquivos de projeto.

---
+++ Site0406/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Site0406.Startup))]
+namespace Site0406
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
